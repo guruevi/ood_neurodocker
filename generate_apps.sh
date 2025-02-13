72d3a529fb332120635e1100a43151544e6d9d0b
Evi Vanoost <vanooste@rcbi.rochester.edu>
2025-02-13 13:30:38 -0500

    Make sure we make the directories before building the containers.

--- generate_apps.sh
+++ generate_apps.sh
@@ -34,10 +34,10 @@ set_title "afni_gui" "AFNI (GUI)"
 for app_version in "${AFNI_VERSIONS[@]}"; do
   echo "Building ${app_name}_${app_version}"
   neurodocker generate ${CONTAINER} \
-    --pkg-manager yum \
-    --base-image fedora:36 \
+    --pkg-manager apt \
+    --base-image debian:bullseye-slim \
     --afni method=binaries version="${app_version}" install_r_pkgs=true \
-    --install supervisor xfce4 xfce4-terminal xterm dbus-x11 dbus-glib vim wget net-tools bzip2 procps-ng mesa-dri-drivers pulseaudio tigervnc-server nss_wrapper gettext \
+    --install supervisor xfce4 xfce4-terminal xterm dbus-x11 libdbus-glib-1-2 vim wget net-tools locales bzip2 procps apt-utils python3-numpy mesa-utils pulseaudio tigervnc-standalone-server libnss-wrapper gettext \
     --run "curl -L --output /usr/bin/ttyd https://github.com/tsl0922/ttyd/releases/download/1.7.7/ttyd.i686" \
     --run "chmod +x /usr/bin/ttyd" \
     --run "echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen && locale-gen" \
@@ -47,6 +47,7 @@ for app_version in "${AFNI_VERSIONS[@]}"; do
     --run "printf '\n# docker-headless-vnc-container:\n\$localhost = \"no\";\n1;\n\' >>/etc/tigervnc/vncserver-config-defaults" \
     --copy template/build/src/vnc_startup.sh /opt/vnc_startup.sh \
   > "bc_${app_name}/${app_name}_${app_version}.${CONTAINER_FILE}"
+  mkdir -p "${CONTAINER_REPOS}/${app_name}"
   if [ "${CONTAINER}" = "docker" ]; then
     # TODO: Build and publish Docker env
     echo "Docker not supported"
@@ -86,20 +87,21 @@ for fsl_version in "${FSL_VERSIONS[@]}"; do
     --run "curl -sL https://github.com/novnc/noVNC/archive/refs/tags/v1.5.0.tar.gz | tar xz --strip 1 -C /opt/novnc" \
     --run "ln -s /opt/novnc/vnc_lite.html /opt/novnc/index.html" \
     --run "printf '\n# docker-headless-vnc-container:\n\$localhost = \"no\";\n1;\n\' >>/etc/tigervnc/vncserver-config-defaults" \
-    --copy template/build/src/vnc_startup.sh /opt/vnc_startup.sh
-  > "bc_fsl/fsl_${fsl_version}.${CONTAINER_FILE}"
+    --copy template/build/src/vnc_startup.sh /opt/vnc_startup.sh \
+  > "bc_${app_name}/${app_name}_${app_version}.${CONTAINER_FILE}"
+  mkdir -p "${CONTAINER_REPOS}/${app_name}"
   if [ "${CONTAINER}" = "docker" ]; then
     # TODO: Build and publish Docker env
     echo "Docker not supported"
   elif [ "${CONTAINER}" = "singularity" ]; then
     # Make sure we don't overwrite the container
-    if [ -f "${CONTAINER_REPOS}/fsl/fsl_"${fsl_version}".sif" ]; then
+    if [ -f "${CONTAINER_REPOS}/${app_name}/${app_name}_${app_version}.sif" ]; then
       echo "Singularity container already exists, skipping"
     else
-      singularity build ${CONTAINER_REPOS}/fsl/fsl_"${fsl_version}".sif bc_fsl/fsl_"${fsl_version}".def
+      singularity build ${CONTAINER_REPOS}/${app_name}/${app_name}_${app_version}.sif bc_${app_name}/${app_name}_${app_version}.def
       echo "Done building Singularity container"
     fi
   fi
-  yq -i '.attributes.app_version.options += [[ "'"${fsl_version}"'", "'"${fsl_version}"'"]]' bc_fsl/form.yml
-  yq -i '.attributes.app_version.options += [[ "'"${fsl_version}"'", "'"${fsl_version}"'"]]' bc_fsl_gui/form.yml
+  yq -i '.attributes.app_version.options += [[ "'"${app_version}"'", "'"${app_version}"'"]]' bc_${app_name}/form.yml
+  yq -i '.attributes.app_version.options += [[ "'"${app_version}"'", "'"${app_version}"'"]]' bc_${app_name}_gui/form.yml
 done
